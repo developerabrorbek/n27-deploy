0af2e60a596ee1242959b4a566278a9d14e86ca3
developerabrorbek <abdulkhamidovabrorbek@gmail.com>
2026-06-23 10:00:03 +0500

    feat: code changes

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port, () => {
-    console.log(`listening on port: ${port}`);
+    console.log(`listening on: ${port}`);
   });
 }
 bootstrap();
